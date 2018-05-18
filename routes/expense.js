411c28791d8d2f494f33fc877d5790285a9f8a16
Luis Henrique Pacheco Paim <peppyno@gmail.com>
2018-05-18 19:05:51 -0300

    Create expense on database

---
+++ routes/expense.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:1234@ds153392.mlab.com:53392/financial-evolve'); // requires setup mlab
+
+router.post('/fixedexpense', (req, res, next) => {
+    const path = '2018.0.expenses.fixedexpense.market';
+    var expense = req.body;
+
+    db.budgets.update({user: 'admin'}, {$set: {path: expense.value}}, function(err, data){
+        if (err) {
+            res.send(err);
+        }
+        res.json(data);
+    });
+});
+
+module.exports = router;
